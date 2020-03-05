2460ea447fd8be0bf366c656743c2b1e1d034b9c
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2020-03-05 16:22:28 +0100

    Added ionic project

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor() {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    /* To make sure we provide the fastest app loading experience 
+       for our users, hide the splash screen automatically 
+       when the app is ready to be used:
+        
+        https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
+    */
+    SplashScreen.hide();
+  }
+}
