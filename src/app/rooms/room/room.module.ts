b43b587a813135af1c51e937e5b9f7e791af3699
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2020-03-22 11:46:34 +0100

    added ngx-autosize package

--- src/app/rooms/room/room.module.ts
+++ src/app/rooms/room/room.module.ts
@@ -1,10 +1,6 @@
 import { SharedModule } from './../../shared.module';
 
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { RoomPageRoutingModule } from './room-routing.module';
 
